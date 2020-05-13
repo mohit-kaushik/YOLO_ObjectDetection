a286c929d4661438da1684b67f824ec56b6ed236
mohit <mohitkaushik2468@gmail.com>
2020-05-13 15:42:58 +0530

    darknet changes upto conv20

--- Darknet.py
+++ Darknet.py
@@ -30,3 +30,16 @@ class YOLO_model(torch.nn.Module):
     self.conv11 = Conv_Block(512,256, kernel_size=1, stride=1, padding=0)
     self.conv12 = Conv_Block(256,512, kernel_size=3, stride=1, padding=1)
 
+    self.conv13 = Conv_Block(512,256, kernel_size=1, stride=1, padding=0)
+    self.conv14 = Conv_Block(256,512, kernel_size=3, stride=1, padding=1)
+    
+    self.conv15 = Conv_Block(512,512, kernel_size=1, stride=1, padding=0)
+    self.conv16 = Conv_Block(512,1024, kernel_size=3, stride=1, padding=1)
+    # "M"
+
+    self.conv17 = Conv_Block(1024,512, kernel_size=1, stride=1, padding=0)
+    self.conv18 = Conv_Block(512, 1024, kernel_size=3, stride=1, padding=1)
+
+    self.conv19 = Conv_Block(1024,512, kernel_size=1, stride=1, padding=0)
+    self.conv20 = Conv_Block(512, 1024, kernel_size=3, stride=1, padding=1)
+
