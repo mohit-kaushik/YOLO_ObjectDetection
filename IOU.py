9c5ae00e51bb24feabe23140fb20da001690a25d
mohit <mohitkaushik2468@gmail.com>
2020-06-03 15:56:08 +0530

    IOU coords calculated

--- IOU.py
+++ IOU.py
@@ -5,4 +5,8 @@ import torch
 def intersection_over_union(box_values, actual_box):
   # box shape (N,4) 4-> (x1,y1), (x2, y2)
   # 1) calculate intersection box coords
+  inter_box_x1 = torch.max(box_values[...,0:1], actual_box[...,0:1])
+  inter_box_y1 = torch.max(box_values[...,1:2], actual_box[...,1:2])
+  inter_box_x2 = torch.min(box_values[...,2:3], actual_box[...,2:3])
+  inter_box_y2 = torch.min(box_values[...,3:4], actual_box[...,3:4])
 
