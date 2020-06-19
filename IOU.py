890cc715fb6d8891ff17b31328e51b3c167acb2b
mohit <mohitkaushik2468@gmail.com>
2020-06-19 15:59:54 +0530

    IOU complete for corner points

--- IOU.py
+++ IOU.py
@@ -13,3 +13,7 @@ def intersection_over_union(box_values, actual_box):
   # when no intersection substraction will be negitive so clamp -ve values to 0.
   inter_box_area = (inter_box_x2 - inter_box_x1).clamp(0) * (inter_box_y2 - inter_box_y1).clamp(0)
 
+  box1_area = abs((box_values[...,2:3] - box_values[...,0:1]) * (box_values[...,3:4] - box_values[...,1:2]))
+  box2_area = abs((actual_box[...,2:3] - actual_box[...,0:1]) * (actual_box[...,3:4] - actual_box[...,1:2]))
+
+  return  inter_box_area / (box1_area + box2_area - inter_box_area)
